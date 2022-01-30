99456e2c7ed85a8fd911cb456750c313d76bd1b5
Александр Мигилев <e-yh@outlook.com>
2022-01-30 17:32:35 +0300

    Добавьте файлы проекта.

---
+++ M2Task14.1/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace M2Task14._1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ObservableCollection<Product> products;
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+           
+            products = new ObservableCollection<Product>();
+
+            products.Add(new Product()
+            {
+                Name = "Сыр",
+                Price = 250,
+                Image = "Data/cheese.png",
+                Type = ProductTypes.Food
+            });
+            
+            products.Add(new Product()
+            {
+                Name = "Пароварка",
+                Price = 1320,
+                Image = "Data/steam_cooker.png",
+                Type = ProductTypes.Appliances
+            });
+
+            products.Add(new Product()
+            {
+                Name = "Мясо",
+                Price = 250,
+                Image = "Data/meat.png",
+                Type = ProductTypes.Food
+            });
+            
+            products.Add(new Product()
+            {
+                Name = "Тостер",
+                Price = 1320,
+                Image = "Data/toaster.png",
+                Type = ProductTypes.Appliances
+            });
+
+            lstBox.ItemsSource = products;
+        }
+    }
+}
---
+++ M2Task14.1/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M2Task14._1
+{
+    public enum ProductTypes
+    {
+        Food,
+        Appliances
+    }
+    internal class Product
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public string Image { get; set; }
+        public ProductTypes Type { get; set; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
